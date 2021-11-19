0b76acb3df4438292d451e27b12146db01811b2d
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-11-19 17:16:47 -0300

    requisito 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,8 @@
-// Coloque aqui suas actions
+export const INPUT_EMAIL = 'INPUT_EMAIL';
+
+export const saveEmail = (payload) => (
+  {
+    type: INPUT_EMAIL,
+    payload,
+  }
+);
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,90 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { saveEmail } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputEmail: '',
+      inputPassword: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.ableButton = this.ableButton.bind(this);
+    this.buttonFunc = this.buttonFunc.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  ableButton() {
+    const { inputEmail, inputPassword } = this.state;
+    const emailRegex = /\S+@\S+\.\S+/;
+    const FIVE = 5;
+    const testEmail = emailRegex.test(inputEmail);
+    const testPassword = inputPassword.length > FIVE;
+    return testEmail && testPassword;
+  }
+
+  buttonFunc(e) {
+    e.preventDefault();
+    const { history, getEmail } = this.props;
+    const { inputEmail } = this.state;
+    getEmail(inputEmail);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { inputEmail, inputPassword } = this.state;
+    return (
+      <section>
+        <label htmlFor="email-input">
+          Email:
+          <input
+            type="email"
+            name="inputEmail"
+            id="email-input"
+            data-testid="email-input"
+            onChange={ this.handleChange }
+            value={ inputEmail }
+          />
+        </label>
+        <label htmlFor="password-input">
+          Password:
+          <input
+            type="password"
+            name="inputPassword"
+            id="password-input"
+            data-testid="password-input"
+            value={ inputPassword }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="button"
+          disabled={ !this.ableButton() }
+          onClick={ this.buttonFunc }
+        >
+          Entrar
+        </button>
+      </section>);
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+  getEmail: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  getEmail: (state) => dispatch(saveEmail(state)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
